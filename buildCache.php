8bd34f1b284b347255a61fc2ee05736fb75cdc53
Bruno Vaula Werneck <brunovaulawerneck@gmail.com>
2018-01-19 15:36:04 +0000

    Fixing buildcache function

--- buildCache.php
+++ buildCache.php
@@ -4,7 +4,7 @@ spl_autoload_register(function($className)
 {
     $namespace=str_replace("\\","/",__NAMESPACE__);
     $className=str_replace("\\","/",$className);
-    $class="./src/".(empty($namespace)?"":$namespace."/")."{$className}.php";
+    $class=__DIR__ . "/src/".(empty($namespace)?"":$namespace."/")."{$className}.php";
     include_once($class);
 });
 
