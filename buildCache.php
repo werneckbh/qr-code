3b6e2786b9e00fdff92e17e6a692bdbb6a5fdf26
Bruno Vaula Werneck <brunovaulawerneck@gmail.com>
2018-01-19 15:31:54 +0000

    Fixing buildcache file

--- buildCache.php
+++ buildCache.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+spl_autoload_register(function($className)
+{
+    $namespace=str_replace("\\","/",__NAMESPACE__);
+    $className=str_replace("\\","/",$className);
+    $class="./src/".(empty($namespace)?"":$namespace."/")."{$className}.php";
+    include_once($class);
+});
 
 use QRCode\Lib\QRTools;
 
